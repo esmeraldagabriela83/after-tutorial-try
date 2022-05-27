38cccfc61ef0a9f929f8d1424b175c77fd8c7de2
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2022-05-27 12:40:28 +0300

    third 27 05 2022

--- js/dog-tutorial.js
+++ js/dog-tutorial.js
@@ -1,26 +1,25 @@
 console.log("-----------------------------------------------------------------------hi from tutorial.js");
-//https://www.youtube.com/watch?v=AVmGmLFcukM    Dogs, JavaScript & An API 🐶 Fetch, Promises & Async Await
+//https://www.youtube.com/watch?v=AVmGmLFcukM    Dogs, JavaScript & An API  Fetch, Promises & Async Await
 
-// fetch("https://dog.ceo/api/breeds/list/all").then(function(response){
-//   return response.json()
-// }).then(function(data){
-//   console.log("---fetch--------------------------------------------------data from tutorial.js about gitHub dogs" , data);
-// })
-
-async function start(){
-  const response=await fetch("https://dog.ceo/api/breeds/list/all");
-  const data=await response.json();
-  console.log("---async await---------------------------------------------------------------data is : " , data);
+fetch("https://dog.ceo/api/breeds/list/all").then(function(response){
+  return response.json()
+}).then(function(data){
+  console.log("---fetch--------------------------------------------------data from tutorial.js about all gitHub dogs" , data);
   createBreedList(data.message)
-}
-//apel functie
-start()
+})
+
+// async function start(){
+//   const response=await fetch("https://dog.ceo/api/breeds/list/all");
+//   const data=await response.json();
+//   console.log("---async await---------------------------------------------------------------data is : " , data);
+//   createBreedList(data.message)
+// }
+// //apel functie //it works only on my computer
+// start()
 //////////////////////////////////////////////////
 function createBreedList(breedList){
  const dogBreed=document.getElementById('breed');
        // dogBreed.style.border="3px solid yellowgreen";
-       dogBreed.style.padding="1em";
-
        dogBreed.innerHTML=`
        <select onchange="loadByBreed(this.value)" style="border:3px solid olive;padding:1em;color:deepskyblue;border-radius:0.5em">
           <option style="color:deepskyblue">Choose a dog breed</option>
@@ -32,17 +31,32 @@ function createBreedList(breedList){
 }
 ///////////////////////////////////////
 
-async function loadByBreed(breed){
+// async function loadByBreed(breed){
+// console.log("breed is: " , breed);
+// if( breed !== `Choose a dog breed`){
+//   console.log("no Choose a dog breed on change when click on options: " , breed);
+//
+//   const response=await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+//   const data=await response.json();
+//   console.log("data from loadByBreed" , data);
+//   createSlideShow(data.message)
+// }
+// }
+
+function loadByBreed(breed){
 console.log("breed is: " , breed);
 if( breed !== `Choose a dog breed`){
-  console.log("no Choose a dog breed on change when click on options: " , breed);
+  console.log("no //Choose a dog breed// on change when click on options: " , breed);
 
-  const response=await fetch(`https://dog.ceo/api/breed/${breed}/images`);
-  const data=await response.json();
-  console.log("data from loadByBreed" , data);
+fetch(`https://dog.ceo/api/breed/${breed}/images`).then(function(response){
+  return response.json()
+}).then(function(data){
+  console.log("---fetch-------data from tutorial.js about specific breed gitHub dogs" , data);
   createSlideShow(data.message)
+});
 }
 }
+
 ////////////////////////////////////////////////////////
 
 function createSlideShow(images){
